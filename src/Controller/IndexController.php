f5de82f08aaf99737c86fc952df3091a89b32ec9
kuba-end <kostek660i@gmail.com>
2021-07-02 00:16:07 +0200

    Files are saving in certain directory

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Photo;
 use App\Form\UploadPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,15 +21,29 @@ class IndexController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()){
             $em = $this->getDoctrine()->getManager();
             if ( $this->getUser()){
-                $entityPhotos = new Photo();
-                $entityPhotos->setFilename($form->get('filename')->getData());
-                $entityPhotos->setIsPublic($form->get('is_public')->getData());
-                $entityPhotos->setUploadedAt(new \DateTime());
-                $entityPhotos->setUser($this->getUser());
-
-                $em->persist($entityPhotos);
-                $em->flush();
-
+                /**
+                 * @var UploadedFile $pictureFileName
+                 */
+                $pictureFileName = $form->get('filename')->getData();
+                if ($pictureFileName){
+                    try {
+                        $orginalFileName = pathinfo($pictureFileName->getClientOriginalName(), PATHINFO_FILENAME );
+                        $safeFileName = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()',$orginalFileName);
+                        $newFileName = $safeFileName.'-'.uniqid().'.'.$pictureFileName->guessExtension();
+                        $pictureFileName->move('images/hosting', $newFileName);
+                        $entityPhotos = new Photo();
+                        $entityPhotos->setFilename($newFileName);
+                        $entityPhotos->setIsPublic($form->get('is_public')->getData());
+                        $entityPhotos->setUploadedAt(new \DateTime());
+                        $entityPhotos->setUser($this->getUser());
+                        $em->persist($entityPhotos);
+                        $em->flush();
+                        $this->addFlash('success','Dodano zdjęcie!');
+                    }
+                    catch(\Exception $e){
+                        $this->addFlash('error','Coś poszło nie tak');
+                    }
+                }
             }
         }
 
---
+++ src/Controller/LatestPhotoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Photo;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LatestPhotoController extends AbstractController{
+
+    #[Route("/latest", name: "latest_photos")]
+    public function index()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $latestPhotos = $em->getRepository(Photo::class)->findBy(['is_public'=>true]);
+         return $this->render('latestPhotos/index.html.twig',[
+             'latestPhotoPublic' => $latestPhotos]);
+    }
+}
